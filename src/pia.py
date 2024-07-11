aae460230febb8881eef2d1067a74e03192c126b
Aashai Avadhani <aashaiavadhani@Aashais-MacBook-Pro.local>
2024-07-11 11:30:06 -0700

    addingpia

--- src/pia.py
+++ src/pia.py
@@ -22,6 +22,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage 
 
 #from langchain.memory import ChatMessageHistory
 #from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
@@ -110,18 +112,151 @@ Easier dev time
 This will generate a privacy impact assessment from scratch!
 Use the template from onetrust
 """
-def develop_privacy_impact_assessment(comp_name, proj_name, project_description, data_dictionary):
+def develop_privacy_impact_assessment(project):
+
+    data_code = project
+    """
+    Get the data from the code and classify into sections
+
+    - develop the P1,P2,P3 level classification for each dataset used 
+    - whether health data was used 
+    - financial or government data used 
+    - demographic data (about a user)
+    - user account data (passwords, username, recovery questions)
+
+    
+
+    from each classification we can determine the privacy risk per
+    then bring up the legislation and statute that the classification will refer t
+    """
+
+    prompt = ""
+    
     pass
 
 
 
 """
 Should return the data classified and why its classified that way
+Inputs: Both strings
+Proj_data is the information about the project
+data_from_code is the data thats used in the project from the code 
 """
-def get_data_collection_response():
-    
+def get_data_classification_response(proj_data, data_from_code):
+    mistral_prompt = """
+    Role: Data Classification Privacy Expert
 
-    pass
+    Get the data from the code and classify into sections
+    <context>
+    {context_code}
+    <context>  
+
+
+    <context>
+    {proj_context}
+    <context>  
+    Data Classification
+        - For every data mentioned in the context determine the classification of each data as either P1,P2,P3 DATA
+        - Example of P2 level data are
+                    Names
+                    Social security numbers (SSN)
+                    Addresses
+                    Phone numbers
+                    Email addresses
+                    Financial account details
+                    Biometric data
+    - develop the P1,P2,P3 level classification for each dataset used 
+
+    - whether health data was used 
+    - financial or government data used 
+    - demographic data (about a user)
+    - user account data (passwords, username, recovery questions)
+
+
+    from each classification we can determine the privacy risk per
+    then bring up the legislation and statute that the classification will refer t
+    """
+    # Define prompt template
+    mistral_prompt = ChatPromptTemplate.from_template("""
+    Role: Lawyer Task
+    <context>
+    {context}
+    <context>                          
+    You are a lawyer tasked with evaluating the data
+                                              
+
+                                              """)
+
+    # Create a retrieval chain to answer questions
+    document_chain = create_stuff_documents_chain(model, mistral_prompt)
+    retrieval_chain = create_retrieval_chain(retriever_final, document_chain)
+    response = retrieval_chain.invoke({"project_context": proj_data, "context_code" : data_from_code})
+    return response["answer"]
+
+    return response
+
+
+def get_data_requirement_documents(legislation_doc, data_from_code):
+
+    embeddings4 = HuggingFaceEmbeddings(model_name='LaBSE')
+    #add a section here just for legal documentation
+    vector_db = FAISS.load_local("faiss_index", embeddings4, allow_dangerous_deserialization=True)
+
+    prompt_template = """
+
+
+    Role: Your task is to determine if the data from a piece of code is 
+
+    Gather the types of data that is relevant to the """ + legislation_doc + """ 
+    documentation.
+
+    Here is the data from the piece of code.
+
+    Task: Your task is to understand if the data used from the code is mentioned in the legislation document 
+    """ + str(data_from_code) + """
+
+    If you are unable to answer the question or pull up relevant information from the legal documents, then answer 
+    "Unable to reach a conclusion for the code compliance issue"
+    
+    """
+
+
+    retriever_final = vector_db.as_retriever(
+    search_type="similarity", search_kwargs={"k": 3} #k: Number of Documents to return, defaults to 4.
+    )
+    # Define LLM
+    model = ChatMistralAI(mistral_api_key= os.environ.get("MISTRAL_API_KEY"), temperature = 0.2)
+    # Define prompt template
+    prompt = ChatPromptTemplate.from_template("""
+    Role: Lawyer Task
+    <context>
+    {context}
+    <context>                          
+    Question you must answer: {input}
+    You are a lawyer tasked with evaluating the data
+                                              
+
+                                              """)
+
+    # Create a retrieval chain to answer questions
+    document_chain = create_stuff_documents_chain(model, prompt)
+    retrieval_chain = create_retrieval_chain(retriever_final, document_chain)
+    response = retrieval_chain.invoke({"context": data_from_code,"input": "List the data that is mentioned in the legislation document " + legislation_doc})
+
+
+    client_mistral = MistralClient(api_key=os.environ.get("MISTRAL_API_KEY"))
+    mistral_model = "mistral-large-latest"
+
+    chat_history = [ChatMessage(role="user", content="Can you give a data classification report on the project?")]
+    
+    response = client_mistral.chat(
+        model=mistral_model, messages=chat_history, tools=tools, tool_choice="auto"
+    )
+
+    return response["answer"]
+
+
+    
 
 def get_data_subject_response():
     pass
@@ -141,24 +276,30 @@ project_object is the code_class
 """
 def answer_pia_questions_individually(project_object,privacy_query):
     print(project_object.display_info())
-    #get the data from the code (should be stored as an object)
+
+
     project_data_code = project_object.data_from_code
-    #per function call for each category, build the agent here for answering PIA questions
-    #Per cateogry have to answer each PIA question
-    data_collection_response = {
+
+
+
+    function_call_data_classification = {
             "type": "function",
             "function": {
-                "name": "retrieve_payment_date",
+                "name": "get_data_classification_response",
                 "description": "Gets classification of data used in the model with legislation",
                 "parameters": {
-                    "type": "object",
+                    "type": "string",
                     "properties": {
-                        "merch_name": {
+                        "proj_data": {
                             "type": "string",
-                            "description": "Text Classification",
+                            "description": "Data Classification Capture",
+                        },
+                        "data_from_code": {
+                            "type": "string",
+                            "description": "data used in the code",
                         }
                     },
-                    "required": ["project_description, data_dictionary"],
+                    "required": ["project_description"],
                 },
             },
         }
@@ -166,13 +307,17 @@ def answer_pia_questions_individually(project_object,privacy_query):
     """
     function calling here with this mistral model 
     """
-    tools = [tool_payment_status, tool_payment_date]
+    tools = [get_data_classification_response]
 
-    response = client.chat(
+    client_mistral = MistralClient(api_key=os.environ.get("MISTRAL_API_KEY"))
+    mistral_model = "mistral-large-latest"
+
+    chat_history = [ChatMessage(role="user", content="Can you give a data classification report on the project?")]
+    
+    response = client_mistral.chat(
         model=mistral_model, messages=chat_history, tools=tools, tool_choice="auto"
     )
-    print(data_collection_response)
 
-    #have the mistral model here pick the function
+    print(response)
 
     pass
