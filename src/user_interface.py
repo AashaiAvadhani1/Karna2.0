3c852c8ef4beef594f8fe1e30c6d89155398ffb4
nvadlamudi1129 <nvadlamudi@uchicago.edu>
2024-07-26 04:13:16 +0500

    Add files via upload

--- src/user_interface.py
+++ src/user_interface.py
@@ -1,4 +1,5 @@
 from typing import List
+import os
 
 import streamlit as st
 from langchain.vectorstores import FAISS
@@ -10,7 +11,9 @@ from src.vector_store import get_conversational_chain
 
 def user_input(user_question: str, history: List[dict]):
     embeddings4 = HuggingFaceEmbeddings(model_name='LaBSE')
-    new_db = FAISS.load_local("faiss_index", embeddings4, allow_dangerous_deserialization=True)
+
+    faiss_index_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'faiss_index')
+    new_db = FAISS.load_local(faiss_index_dir, embeddings4, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
 
     chain = get_conversational_chain()
